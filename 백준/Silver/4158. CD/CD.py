b4665c10fe10b05fb9ae2b61a0c12ba80a34beba
Sujeong Shin <71260043+shin-sj@users.noreply.github.com>
2023-01-27 16:38:26 +0900

    [Silver V] Title: CD, Time: 1680 ms, Memory: 220732 KB -BaekjoonHub

---
+++ 백준/Silver/4158. CD/CD.py
@@ -0,0 +1,48 @@
+import sys
+input = sys.stdin.readline
+
+while True :
+    n, m = map(int, input().split()) #n-상근이가 갖고 있는 cd수, m-선영이가 갖고 있는 cd수 
+    nlist = [] #상근이가 갖고 있는 cd수 
+    mlist = [] #선영이가 갖고 있는 cd수 
+    if n == 0 and m == 0 :
+        break
+    for _ in range(n) :
+        nlist.append(int(input())) #오름차순으로 주어짐
+    for _ in range(m) :
+        mlist.append(int(input()))
+        
+    ## 1. bruteforce ## 
+    # cnt = 0
+    # for nn in nlist :
+    #     if nn in mlist :
+    #         cnt += 1
+    # print(cnt)
+    
+    ## 2. binary search ##
+    cnt = 0
+    for mm in mlist : 
+        lt = 0
+        rt = n - 1
+        while lt <= rt :
+            mid = (lt+rt)//2 
+            if nlist[mid] == mm :
+                cnt += 1
+                break
+            elif nlist[mid] > mm :
+                rt = mid - 1
+            elif nlist[mid] < mm :
+                lt = mid + 1
+    print(cnt)
+                
+    ##3. 딕셔너리 ##
+    # ndict = {}
+    # mdict = {}
+    # cnt = 0
+    # for _ in range(n) :
+    #     ndict[int(input())] = 1
+    # for _ in range(m) :
+    #     tmp = int(input())
+    #     if ndict.get(tmp) == 1 :
+    #         cnt += 1
+    # print(cnt)
