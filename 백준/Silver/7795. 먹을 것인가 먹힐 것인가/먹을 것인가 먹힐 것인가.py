623933f26f3256506bd29ff970cf0125e9a33378
Sujeong Shin <71260043+shin-sj@users.noreply.github.com>
2023-02-01 14:33:57 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 276 ms, Memory: 34684 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -1,29 +1,26 @@
 import sys
 input = sys.stdin.readline
-
-def binary_search(target, blist):
-    lt = 0 #시작 
-    rt = len(blist)-1 #인덱스로 계산할 것이기 때문. 
-    result = -1 # -1이어야 하는 이유? 개수를 구해야 하기 때문에 인덱스 +1을 해줘야 한다, 그러나 blist에 target보다 작은 수가 없을 경우에는 계속해서 +1이 되어 버림. 따라서 -1 + 1 = 0이기 위해 초기값을 -1로 두는 것. 
-
-    while lt <= rt: 
-        mid = (lt + rt) // 2 #index 
-        if blist[mid] < target :
+def binary_search(target, blist) :
+    lt = 0
+    rt = len(blist) - 1 
+    result = -1
+    while lt <= rt :
+        mid = (rt + lt) // 2 #인덱스! 
+        if target > blist[mid] :
             result = mid
             lt = mid + 1
-        else:
-            rt = mid -1
-    return result
-
+        else : 
+            rt = mid - 1
+    return result  
+    
 tc = int(input())
-for _ in range(tc):
-    N, M = map(int, input().split())
+for _ in range(tc) :
+    n, m = map(int, input().split())
     alist = list(map(int, input().split()))
     blist = list(map(int, input().split()))
     alist.sort()
     blist.sort()
-    cnt = 0 
-    for i in alist :
-        cnt += binary_search(i, blist) + 1
-
-    print(cnt)
+    ans = 0
+    for a in alist :
+        ans += binary_search(a, blist) + 1
+    print(ans)
