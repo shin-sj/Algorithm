208ee154beb3ffa3ebfae6e63d2b9395c1793867
Sujeong Shin <71260043+shin-sj@users.noreply.github.com>
2023-02-06 16:21:23 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 4256 ms, Memory: 191016 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,37 +1,36 @@
+#벽부수고이동하기. 
 import sys
 from collections import deque
 input = sys.stdin.readline
-dx = [0,0,-1,1]
-dy = [1,-1,0,0]
-
-def bfs(a, b, c) :
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+def bfs(i, j, cnt) :
     dQ = deque()
-    dQ.append((a, b, c))
+    dQ.append((i, j, cnt))
     
     while dQ :
-        x, y, z =dQ.popleft()
-        
+        x, y, c = dQ.popleft()
         if x == n-1 and y == m-1 :
-            return visited[x][y][z]
-        
+            return visited[x][y][c] 
         for i in range(4) :
             xx = x + dx[i]
             yy = y + dy[i]
             
             if xx < 0 or xx >= n or yy < 0 or yy >= m :
                 continue
-            #벽 + 부술 수 있는 경우
-            if board[xx][yy] == 1 and z == 0 :
+            
+            if c == 0 and board[xx][yy] == 1 :
                 visited[xx][yy][1] = visited[x][y][0] + 1
                 dQ.append((xx, yy, 1))
-            elif board[xx][yy] == 0 and visited[xx][yy][z] == 0 :
-                visited[xx][yy][z] = visited[x][y][z] + 1
-                dQ.append((xx, yy, z))
+                
+            elif board[xx][yy] == 0 and visited[xx][yy][c] == 0 :
+                visited[xx][yy][c] = visited[x][y][c] + 1
+                dQ.append((xx, yy, c))
     return -1
-    
+                
+        
 n, m = map(int, input().split())
 board = [list(map(int, input().rstrip())) for _ in range(n)]
 visited = [[[0] * 2 for _ in range(m)] for _ in range(n)]
-# print(visited)
-visited[0][0][0] = 1 #시작 방문 표시
+visited[0][0][0] = 1
 print(bfs(0, 0, 0))
