213f4fece69622e3fd4e0daa770cef5776396dde
Sujeong Shin <71260043+shin-sj@users.noreply.github.com>
2023-02-07 12:22:28 +0900

    [Bronze II] Title: 부재중 전화, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1333. 부재중 전화/부재중 전화.py
+++ 백준/Bronze/1333. 부재중 전화/부재중 전화.py
@@ -1,17 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-N, L, D = map(int, input().split())
-total = N * L + (N - 1) * 5 # 앨범의 총 길이
-song_time = [False] * total #노래 나오는 시간 체크 
+n, l, d = map(int, input().split())
+total_time = (n*l) + (n-1)*5
+song_time = [False] * total_time
 
-for i in range(0, total, L + 5): # 각 노래는 0 초부터 시작해서 L + 5 초마다 나온다.
-    for j in range(i, i + L): # 시작하는 부분 i부터 노래가 끝나는 부분인 i + L 전까지 체크
+#노래 나오는 시간 - True
+for i in range(0, total_time, l+5) :
+    for j in range(i, l+i) :
         song_time[j] = True
-        
-for i in range(0, total, D): # 0초부터 D초마다 노래가 나오는지 체크
-    if song_time[i] == False:
+
+for i in range(0, total_time, d) :
+    if song_time[i] == False :
         print(i)
         break
-else:
-    print(i + D)
+else :
+    print(i + d)
