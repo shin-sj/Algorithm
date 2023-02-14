6b17ea5cdb8374edb961f8264dc81febb71d8a00
Sujeong Shin <71260043+shin-sj@users.noreply.github.com>
2023-02-14 13:04:49 +0900

    [Bronze II] Title: 부재중 전화, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Bronze/1333. 부재중 전화/부재중 전화.py
+++ 백준/Bronze/1333. 부재중 전화/부재중 전화.py
@@ -1,18 +1,20 @@
+#부재중 전화
 import sys
 input = sys.stdin.readline
 
-n, l, d = map(int, input().split())
-total_time = (n*l) + (n-1)*5
-song_time = [False] * total_time
+N, L, D = map(int, input().split())
+#N - 노래의 개수
+#L - 노래의 길이
+#D - 전화벨은 D초에 한번씩 울린다. 
+total = (N*L) + (N-1)*5
+board = [0] * total 
 
-#노래 나오는 시간 - True
-for i in range(0, total_time, l+5) :
-    for j in range(i, l+i) :
-        song_time[j] = True
-
-for i in range(0, total_time, d) :
-    if song_time[i] == False :
+for i in range(0, total, L+5) :
+    for j in range(i, i+L) :
+        board[j] = 1 #노래 나오는 시간 체크
+for i in range(0, total, D) :
+    if board[i] == 0 :
         print(i)
         break
 else :
-    print(i + d)
+    print(i+D)
