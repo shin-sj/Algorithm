3ca1c0d284730fc6cfabb841e7d81cbef12934ea
Sujeong Shin <71260043+shin-sj@users.noreply.github.com>
2023-02-22 10:15:28 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 188 ms, Memory: 119376 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -1,26 +1,27 @@
+#먹을 것인가 먹힐 것인가. 
 import sys
 input = sys.stdin.readline
-def binary_search(target, blist) :
-    lt = 0
-    rt = len(blist) - 1 
-    result = -1
-    while lt <= rt :
-        mid = (rt + lt) // 2 #인덱스! 
-        if target > blist[mid] :
-            result = mid
-            lt = mid + 1
-        else : 
-            rt = mid - 1
-    return result  
-    
-tc = int(input())
+
+tc = int(input()) #테스트케이스
 for _ in range(tc) :
     n, m = map(int, input().split())
     alist = list(map(int, input().split()))
     blist = list(map(int, input().split()))
     alist.sort()
     blist.sort()
-    ans = 0
+    cnt = 0
+
     for a in alist :
-        ans += binary_search(a, blist) + 1
-    print(ans)
+        lt = 0
+        rt = len(blist) - 1
+        result = -1
+        
+        while lt <= rt :
+            mid = (rt+lt) // 2  #index
+            if blist[mid] < a :
+                result = mid 
+                lt = mid + 1
+            else :
+                rt = mid - 1
+        cnt += result + 1
+    print(cnt)
