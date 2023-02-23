10d9d79bf5b1614bb22fa5b60a0a7559da1019ca
Sujeong Shin <71260043+shin-sj@users.noreply.github.com>
2023-02-23 12:29:44 +0900

    [Silver IV] Title: 카드, Time: 144 ms, Memory: 45092 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.py
+++ 백준/Silver/11652. 카드/카드.py
@@ -2,10 +2,10 @@ import sys
 from collections import Counter
 input = sys.stdin.readline
 
-N = int(input()) #준규가 가지고 있는 숫자카드의 개수 N
-alist = []
-for _ in range(N) :
-    alist.append(int(input()))
-alist.sort()
-acount = Counter(alist).most_common(1)
-print(acount[0][0])
+n = int(input()) #준규가 가지고 있는 숫자카드의 개수
+cards = []
+for _ in range(n) :
+    cards.append(int(input()))
+cards.sort()
+adict = Counter(cards).most_common(1)
+print(adict[0][0])
