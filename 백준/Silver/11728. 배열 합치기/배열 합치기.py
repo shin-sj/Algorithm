b7e15cdbee8f3428974554c6b050e9e05d7e1637
Sujeong Shin <71260043+shin-sj@users.noreply.github.com>
2023-02-23 12:01:53 +0900

    [Silver V] Title: 배열 합치기, Time: 2416 ms, Memory: 200040 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.py
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -4,24 +4,20 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 alist = list(map(int, input().split()))
 blist = list(map(int, input().split()))
+lt, rt = 0, 0
 answer = []
-aIdx, bIdx = 0, 0
-# pointer을 이동시키면서 최대한 합치기
-while aIdx < n and bIdx < m :
-    if alist[aIdx] < blist[bIdx] :
-        answer.append(alist[aIdx])
-        aIdx += 1
+while lt < n and rt < m:
+    if alist[lt] < blist[rt] :
+        answer.append(alist[lt])
+        lt += 1
     else :
-        answer.append(blist[bIdx])
-        bIdx += 1
-# 남은 배열 합치기
-# a 배열과 b 배열의 길이가 같지 않으면 1번의 과정이 끝난 뒤 남는 배열이 생길 수 있다.
-while aIdx < n :
-    answer.append(alist[aIdx])
-    aIdx += 1
-while bIdx < m :
-    answer.append(blist[bIdx])
-    bIdx += 1
-
-for a in answer :
-    print(a, end=' ')
+        answer.append(blist[rt])
+        rt += 1
+while lt < n :
+    answer.append(alist[lt])
+    lt += 1
+while rt < m :
+    answer.append(blist[rt])
+    rt += 1
+for a in answer:
+    print(a, end = ' ')
