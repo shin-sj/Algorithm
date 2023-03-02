b52f0ec331a96650c980efdd05e9e6a4220635e2
Sujeong Shin <71260043+shin-sj@users.noreply.github.com>
2023-03-02 13:57:11 +0900

    [Silver V] Title: 배열 합치기, Time: 1372 ms, Memory: 342244 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.py
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -4,20 +4,23 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 alist = list(map(int, input().split()))
 blist = list(map(int, input().split()))
-lt, rt = 0, 0
-answer = []
-while lt < n and rt < m:
+ans = []
+lt = 0
+rt = 0
+
+while lt < n and rt < m :
     if alist[lt] < blist[rt] :
-        answer.append(alist[lt])
+        ans.append(alist[lt])
         lt += 1
     else :
-        answer.append(blist[rt])
+        ans.append(blist[rt])
         rt += 1
 while lt < n :
-    answer.append(alist[lt])
+    ans.append(alist[lt])
     lt += 1
 while rt < m :
-    answer.append(blist[rt])
+    ans.append(blist[rt])
     rt += 1
-for a in answer:
+for a in ans :
     print(a, end = ' ')
+        
