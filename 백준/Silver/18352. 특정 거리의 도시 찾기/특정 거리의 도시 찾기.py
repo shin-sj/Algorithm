ccee09f1ab83e95cf6fb00f08d300c147645a115
Sujeong Shin <71260043+shin-sj@users.noreply.github.com>
2023-03-03 13:20:56 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1788 ms, Memory: 98736 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,34 +1,35 @@
-#특정 거리의 도시 찾기
 import sys
 from collections import deque
 input = sys.stdin.readline
-def dfs(start) :
+def bfs(i) : 
     dQ = deque()
-    dQ.append(start)
-    visited[start] = 1
-    cnt = 0
-    while dQ:
-        s = dQ.popleft()
-        for i in graph[s] :
-            if visited[i] == 0 :
-                visited[i] = 1 #방문 표시 
-                distance[i] = distance[s] + 1
-                dQ.append(i)
+    dQ.append(i)
+    
+    while dQ :
+        x = dQ.popleft()
+        for j in board[x] :
+            if visited[j] == -1 :
+                visited[j] = visited[x] + 1
+                dQ.append(j)
+    
+# n-도시의 개수, m-도로의 개수, k-거리 정보, x-출발도시의 번호
+n, m, k, x = map(int, input().split()) 
+board = [[] for _ in range(n+1)]
 
-#n도시의개수, m도로의개수, k거리정보, x출발도시정보
-n, m, k, x = map(int, input().split())
-graph = [[] for _ in range(n+1)]
-distance = [0] * (n+1)
-visited = [False] * (n+1)
-# print(graph)
 for _ in range(m) :
-    a,b = map(int, input().split())
-    graph[a].append(b)
-dfs(x)
+    # a -> b 단방향 도로 
+    a, b = map(int, input().split())
+    board[a].append(b)
+
+# X로부터 출발하여 도달할 수 있는 도시 중에서
+# 최단 거리가 K인 모든 도시의 번호
+visited = [-1] * (n+1)
+visited[x] = 0
+bfs(x)
 flag = 0
-for i in range(1, n+1) :
-    if distance[i] == k :
+for i in range(1, len(visited)) :
+    if visited[i] == k :
         print(i)
         flag = 1
-if flag ==0 :
+if flag == 0 :
     print(-1)
