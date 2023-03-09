15e4f05f117d475102bd5374d3febfc6dc9451d9
Sujeong Shin <71260043+shin-sj@users.noreply.github.com>
2023-03-09 15:12:21 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.34 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844#qna) 
 
 ### 성능 요약
 
-메모리: 54.5 MB, 시간: 3.71 ms
+메모리: 52.3 MB, 시간: 4.34 ms
 
 ### 구분
 
--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,14 +2,16 @@ import java.util.*;
 class Solution {
     int[] dx = {0,0,1,-1};
     int[] dy = {1,-1,0,0};
+    
     public int solution(int[][] maps) {
         int answer = 0;
         int[][] visited = new int[maps.length][maps[0].length];
-       
+        // System.out.println(visited);
         bfs(maps, visited);
         answer = visited[maps.length-1][maps[0].length-1];
+        
         if(answer == 0) 
-            return -1;
+            return -1; 
         return answer;
     }
     public void bfs(int[][] maps, int[][] visited) {
@@ -23,19 +25,18 @@ class Solution {
             int[] current = queue.poll();
             int cx = current[0];
             int cy = current[1];
-            for(int i = 0; i < 4;i++) {
+            for(int i = 0; i < 4; i++) {
                 int xx = cx + dx[i];
                 int yy = cy + dy[i];
                 
-                if(xx < 0 || xx >= maps.length || yy < 0 || yy >= maps[0].length)
+                if(xx < 0 || xx >= maps.length || yy < 0 || yy >= maps[0].length) 
                     continue;
-                if(maps[xx][yy] == 1 && visited[xx][yy] == 0) {
+                if(maps[xx][yy] == 1 && visited[xx][yy] == 0 ) {
                     visited[xx][yy] = visited[cx][cy] + 1;
                     queue.add(new int[]{xx, yy});
+                    
                 }
             }
-            
         }
-
     }
 }
